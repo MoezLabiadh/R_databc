955a15481139f7974c8277f1f2e2993334b6fda0
MoezLabiadh <60794110+MoezLabiadh@users.noreply.github.com>
2022-02-04 13:54:51 -0800

    Add files via upload

---
+++ bcdata_intersect_geoData.R
@@ -0,0 +1,38 @@
+library(bcdata)
+library(sf)
+library(dplyr)
+library(writexl)
+
+
+
+# Get land districts and filter based on conditions
+districts <- bcdc_query_geodata("WHSE_TANTALIS.TA_LAND_DISTRICTS_SVW") %>%
+  
+            filter(LAND_DISTRICT_NAME %in% c("NANAIMO DISTRICT","SAYWARD DISTRICT")) %>%
+  
+            collect()
+
+
+
+# Get tenures intersecting with districts
+tenures <- bcdc_query_geodata("WHSE_TANTALIS.TA_CROWN_TENURES_SVW") %>%
+           filter(TENURE_PURPOSE == "AQUACULTURE"&
+                  TENURE_STAGE == "APPLICATION" &
+                  APPLICATION_TYPE_CDE == "NEW") %>%
+
+           collect()
+
+
+intersect <- tenures %>%
+            st_join(districts, left = FALSE)
+
+
+write_xlsx(intersect,"\\\\sfp.idir.bcgov\\S164\\S63087\\Share\\FrontCounterBC\\Moez\\Training\\R\\myCodes\\outputs\\export.xlsx")
+
+
+# Plot Results
+intersect  %>%
+  ggplot() +
+  geom_sf() +
+  theme_minimal()
+
