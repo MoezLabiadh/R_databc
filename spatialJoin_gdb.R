a9395be736073319a136bde27b6e4934e2525335
MoezLabiadh <60794110+MoezLabiadh@users.noreply.github.com>
2022-02-10 15:38:32 -0800

    Update and rename spatialJoin_dcdata_gdb.R to spatialJoin_gdb.R

--- spatialJoin_gdb.R
+++ spatialJoin_gdb.R
@@ -5,13 +5,13 @@ library (ggplot2)
 library(writexl)
 
 # Read Harvest Area feature class
-ha_gdb <- "\\\\sfp.idir.bcgov\\S164\\S63087\\Share\\FrontCounterBC\\Moez\\WORKSPACE\\20211125_aquaPlants_FN_consult\\data.gdb"
+ha_gdb <- "\\\data.gdb"
 ha_n <- "Harvest_areas_2022_all"
 
 ha <- sf::st_read(ha_gdb, layer = ha_n)
 
 # Read Rockfish areas feature class
-rf_gdb <- "\\\\sfp.idir.bcgov\\S164\\S63087\\Share\\FrontCounterBC\\Moez\\WORKSPACE\\20211018_aquaPlants_dfo\\2022HarvestAreas_mapping\\harvest_areas_working.gdb"
+rf_gdb <- "\\harvest_areas_working.gdb"
 rf_n <- "aoi_DFO_ROCKFISH_CONS_AREA_SVW_2020"
 
 rf <- sf::st_read(rf_gdb, layer = rf_n)
@@ -30,5 +30,5 @@ intersect <- ha %>%
 
 join <- st_join(ha, rf, join=st_intersects, left = FALSE)
 
-write_xlsx(join,"\\\\sfp.idir.bcgov\\S164\\S63087\\Share\\FrontCounterBC\\Moez\\Training\\R\\myCodes\\outputs\\join.xlsx")
+write_xlsx(join,"\\join.xlsx")
 
